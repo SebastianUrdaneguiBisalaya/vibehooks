89f939c5ac020430d501bfb5799cb1429f9d331f
SebastianUrdaneguiBisalaya <sebasurdanegui@gmail.com>
2026-01-03 15:33:38 -0500

    chore: add export

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
-export { useScreenSize } from './useScreenSize';
 export { useIntersectionObserver } from './useIntersectionObserver';
+export { useIsClient } from './useIsClient';
 export { useIsDesktop } from './useIsDesktop';
+export { useScreenSize } from './useScreenSize';
