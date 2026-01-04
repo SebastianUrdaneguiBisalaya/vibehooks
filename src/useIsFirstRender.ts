cb365e866176e6ef448af1786ce928e61f02abea
SebastianUrdaneguiBisalaya <sebasurdanegui@gmail.com>
2026-01-03 23:23:21 -0500

    chore: add useIsFirstRender hook

---
+++ src/useIsFirstRender.ts
@@ -0,0 +1,26 @@
+import * as React from 'react';
+
+/**
+ * `useIsFirstRender` returns true if the component is the first render.
+ * 
+ * @returns A boolean value indicating if the component is the first render.
+ * 
+ * @example
+ * ```tsx
+ * const isFirstRender = useIsFirstRender();
+ * ```
+ * 
+ * @author Sebastian Marat Urdanegui Bisalaya <sebastianurdanegui.com>
+ * 
+ * @since 0.0.1
+ * @version 0.0.1
+ * 
+ */
+export function useIsFirstRender(): boolean {
+    const isFirstRender = React.useRef(true);
+
+    const value = isFirstRender.current;
+    isFirstRender.current = false;
+
+    return value;
+}
