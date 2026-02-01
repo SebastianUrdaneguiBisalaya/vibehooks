5a40cf11cf2634483805362e01acbe9d8a54f9e7
SebastianUrdaneguiBisalaya <sebasurdanegui@gmail.com>
2026-02-01 16:49:14 -0500

    chore: improve hook and ui

--- src/useServerSentEvent.ts
+++ src/useServerSentEvent.ts
@@ -37,8 +37,15 @@ export interface UseServerSentEventResult {
 	/**
 	 * Current connection state.
 	 */
-	readyState: number | null;
+	readyState: SSEReadyState | null;
 }
+
+export type SSEReadyState = 0 | 1 | 2;
+/**
+ * 0 = CONNECTING
+ * 1 = OPEN
+ * 2 = CLOSED
+ */
 
 /**
  * `useServerSideEvent` provides unopinionated access to Server-Sent Event (SSE) via the EventSource Web API.
@@ -73,7 +80,11 @@ export function useServerSentEvent(
 	} = options;
 
 	const sourceRef = React.useRef<EventSource | null>(null);
-	const [readyState, setReadyState] = React.useState<number | null>(null);
+	const [readyState, setReadyState] = React.useState<SSEReadyState | null>(null);
+
+  const onMessageRef = React.useRef(onMessage);
+  const onOpenRef = React.useRef(onOpen);
+  const onErrorRef = React.useRef(onError);
 
 	const isSupported =
 		typeof window !== 'undefined' && typeof EventSource !== 'undefined';
@@ -84,31 +95,37 @@ export function useServerSentEvent(
 		setReadyState(null);
 	}, []);
 
+  React.useEffect(() => {
+    onMessageRef.current = onMessage;
+    onOpenRef.current = onOpen;
+    onErrorRef.current = onError;
+  }, [onMessageRef, onOpenRef, onErrorRef]);
+
 	React.useEffect(() => {
 		if (!isSupported || !enabled) return;
 		const source = new EventSource(url, { withCredentials });
 		sourceRef.current = source;
 
 		source.onopen = event => {
-			setReadyState(source.readyState);
-			onOpen?.(event);
+			setReadyState(source.readyState as SSEReadyState);
+			onOpenRef.current?.(event);
 		};
 
 		source.onmessage = event => {
-			setReadyState(source.readyState);
-			onMessage?.(event);
+			setReadyState(source.readyState as SSEReadyState);
+			onMessageRef.current?.(event);
 		};
 
 		source.onerror = event => {
-			setReadyState(source.readyState);
-			onError?.(event);
+			setReadyState(source.readyState as SSEReadyState);
+			onErrorRef.current?.(event);
 		};
 
 		return () => {
 			source.close();
 			sourceRef.current = null;
 		};
-	}, [url, enabled, withCredentials, onOpen, onMessage, onError, isSupported]);
+	}, [url, enabled, withCredentials, isSupported]);
 
 	return {
 		close,
--- website/src/app/docs/connectivity-and-async/useServerSentEvent/Demo.tsx
+++ website/src/app/docs/connectivity-and-async/useServerSentEvent/Demo.tsx
@@ -1,12 +1,19 @@
 'use client';
 
 import { useServerSentEvent } from "../../../../../../src";
+import { Button } from "@/components/ui/Button";
+import { Tag } from "@/components/ui/Tag";
 import { Layout } from "@/layouts/Layout";
 
 export default function Demo() {
+  const { close, readyState } = useServerSentEvent(
+    'https://echo.websocket.org/.sse',
+  )
   return (
     <Layout>
       <Layout.Title>Server-Sent Event</Layout.Title>
+      <Tag.Primary>Status: {readyState}</Tag.Primary>
+      <Button.Destructive onClick={close}>Close</Button.Destructive>
     </Layout>
   )
 }
