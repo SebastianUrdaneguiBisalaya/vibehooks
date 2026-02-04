778f0c660d3ffbc5d8981999367c37a723836d19
SebastianUrdaneguiBisalaya <sebasurdanegui@gmail.com>
2026-02-04 00:20:40 -0500

    chore: change config

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -3,7 +3,10 @@ import { defineConfig } from 'tsdown';
 export default defineConfig({
 	clean: true,
 	dts: true,
-	entry: ['src/index.ts'],
+	entry: 'src/**/*.ts',
+  exports: {
+    all: true,
+  },
 	platform: 'browser',
 	unbundle: true,
 });
