11598663248f1c19c87b8449c94f000b82556c3c
SebastianUrdaneguiBisalaya <sebasurdanegui@gmail.com>
2026-02-05 00:23:15 -0500

    chore: change structure of output format

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -2,23 +2,14 @@ import { defineConfig } from 'tsdown';
 
 export default defineConfig({
 	clean: true,
-	dts: true,
-	entry: 'src/**/*.ts',
+	dts: {
+    sourcemap: false,
+  },
+	entry: 'src/index.ts',
   exports: {
-    all: true,
-    customExports(pkg) {
-      for (const [key, value] of Object.entries(pkg)) {
-        if (typeof value === 'string' && value.endsWith('.js')) {
-          const dts = value.replace(/\.js$/, '.d.ts');
-          pkg[key] = {
-            import: value,
-            types: dts,
-          };
-        }
-      }
-      return pkg;
-    },
+    all: false
   },
 	platform: 'browser',
-	unbundle: true,
+  sourcemap: false,
+	unbundle: false,
 });
